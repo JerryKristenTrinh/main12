9752c23a6cc4eada3f0dba603b518afc3ee841ad
JerryKristenTrinh <165662290+JerryKristenTrinh@users.noreply.github.com>
2025-04-17 22:25:27 +0700

    main12.py

--- main12.py
+++ main12.py
@@ -74,7 +74,7 @@ if "start_time" not in st.session_state:
 
 url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 
-title = "English vocabulary practice test"
+title = "English vocabulary practice game"
 title = nice_present("center", 50, title, "bold")
 
 input_name = st.text_input("Type your name:")
