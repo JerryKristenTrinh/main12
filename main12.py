05f1fd692c44e2873d2d2c0e18f204cbe724e4d1
JerryKristenTrinh <165662290+JerryKristenTrinh@users.noreply.github.com>
2025-04-19 13:42:34 +0700

    main12.py

--- main12.py
+++ main12.py
@@ -190,9 +190,9 @@ suggest_word = nice_present("left", 15, suggest_word, "italian")
 
 if input_name.lower() == "rick astley":
     st.video(url, autoplay=True)
-elif amount_word >= 70:
+if amount_word >= 70:
     st.video(url2, autoplay=True)
-elif str(phone).lower() in ["30/4", "30-4", "30.4", "30 4", "30 / 4", "30_4", "30 - 4"]:
+if str(phone).lower() in ["30/4", "30-4", "30.4", "30 4", "30 / 4", "30_4", "30 - 4"]:
     st.video(url3, autoplay=True)
 
 if input_name and phone and locate and amount_word:
